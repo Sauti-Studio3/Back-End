d6857da4adc768f8ea0f54fcf30bcf282ac028f7
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-02-02 12:44:36 -0800

    Switch from postgres to sqlite3

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,16 @@ require('dotenv').config();
 module.exports = {
 
   development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/sauti.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     migrations: {
       directory: './data/migrations'
     },
