b9a79ee497d20f79cf943b3a1735c7d4a5bf195e
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-02-04 13:21:40 -0800

    Add POST flow endpoint

--- middleware/validate-body-middleware.js
+++ middleware/validate-body-middleware.js
@@ -27,21 +27,29 @@ function checkMissingFields(reqBody, endpoint) {
   return missingFields;
 }
 
-function validateBody(req, res, next) {
-  if (Object.entries(req.body).length > 0) {
-    const endpoint = req.url.replace('/', '') //Get rid of / in URL
-    const missingFields = checkMissingFields(req.body, endpoint);
-    if (missingFields.length > 0) {
-      res.status(400).json({
-        message: `Request is missing the following required field(s): ${missingFields.join(', ')}`
-      });
-    } else {
+function validateBody(endpoint) {
+    return (req, res, next) => {
+    if (req.method === 'GET' || req.method === 'DELETE') {
       next();
+    } else {
+      if (Object.entries(req.body).length > 0) {
+        // const endpoint = req.url.replace('/', '') //Get rid of / in URL
+        console.log(endpoint);
+        const missingFields = checkMissingFields(req.body, endpoint);
+        if (missingFields.length > 0) {
+          res.status(400).json({
+            message: `Request is missing the following required field(s): ${missingFields.join(', ')}`
+          });
+        } else {
+          next();
+        }
+      } else {
+        res.status(400).json({
+          message: 'Request is missing body data.'
+        })
+      } 
     }
-  } else {
-    res.status(400).json({
-      message: 'Request is missing body data.'
-    })
+    
   }
 }
 
