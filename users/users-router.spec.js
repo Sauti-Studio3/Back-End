24f90e9d722d799bd0ec7839b7c9c3402095718f
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-02-06 09:06:22 -0800

    Test for users-router

---
+++ users/users-router.spec.js
@@ -0,0 +1,84 @@
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../data/db-config');
+
+describe('users-router', () => {
+  beforeAll(async () => {
+    await db('flows').truncate();
+  })
+  let token;
+  let userId;
+  beforeAll(done => {
+    request(server)
+      .post('/api/auth/login')
+      .send({
+        username: 'leni',
+        password: 'hauer'
+      })
+      .end((err, res) => {
+        token = res.body.token;
+        userId = res.body.user.id;
+        done();
+      })
+  })
+
+  describe('POST /:id/flows', () => {
+
+    it('returns 400 Bad Request if missing \'name\' field', () => {
+      request(server)
+        .post(`/api/users/${userId}/flows`)
+        .set('Authorization', token)
+        .send({
+          nothing: 'I am an invalid request'
+        })
+        .then(res => {
+          expect(res.status).toBe(400)
+        });
+    });
+
+    it('returns an object with a \'name\' property', () => {
+      request(server)
+        .post(`/api/users/${userId}/flows`)
+        .set('Authorization', token)
+        .send({
+          name: 'a new flow'
+        })
+        .then(res => {
+          expect(res.body).toHaveProperty('name'); 
+        });
+    });
+  });
+
+  describe('GET /:id/flows', () => {
+
+    // beforeAll(done => {
+    //   request(server)
+    //   .get(`/api/users/${userId}/flows`)
+    //   .set('Authorization', token)
+    //   .end((err, res) => {
+    //     it('returns 200 OK', () => {
+    //       expect(res.status).toBe(300);
+    //     })
+    //     done();
+    //   })
+    // })
+
+    it('returns 200 OK', () => {
+      request(server)
+        .get(`/api/users/${userId}/flows`)
+        .set('Authorization', token)
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it('returns an array of 1 element', () => {
+      request(server)
+        .get(`/api/users/${userId}/flows`)
+        .set('Authorization', token)
+        .then(res => {
+          expect(res.body).toHaveLength(1);
+        })
+    })
+  })
+})
