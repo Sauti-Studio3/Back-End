12b651999221c38121d4b82b041743d966bc4b73
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-06-05 10:20:10 -0700

    deletes user-router comments

--- users/users-router.js
+++ users/users-router.js
@@ -11,14 +11,6 @@ router.get('/:id/flows', validateUserId, restrictUser('users'), (req, res) => {
   const { id } = req.params;
   Flows.findByUserId(id)
     .then(flows => {
-      // const flowsWithPages = flows.map(async flow => { //TODO: Ask Jimmy (if time);
-      //   let pages = await Pages.findByFlowId(flow.id);
-      //   console.log(flow, pages);
-      //   return {
-      //     ...flow,
-      //     pages: pages
-      //   }
-      // })
       res.status(200).json(flows);
     })
     .catch(error => {
@@ -64,6 +56,5 @@ router.use('/flows', flowsRouter);
 router.use('/pages', pagesRouter);
 router.use('/options', optionsRouter);
   
-
 module.exports = router;
 
